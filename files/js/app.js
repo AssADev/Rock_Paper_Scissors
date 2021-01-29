04c76093603b065b86b63e32710522c4b04aa30d
AssADev <gravelteo.pro@gmail.com>
2021-01-29 16:33:09 +0100

    Interface Game create

--- files/js/app.js
+++ files/js/app.js
@@ -1,6 +1,21 @@
 // Start game :
 const start_btn = document.getElementById("start");
+const back_btn = document.getElementById("back_btn");
 
 start_btn.addEventListener("click", () => {
     document.body.classList.add("ingame");
 });
+
+back_btn.addEventListener("click", () => document.body.classList.remove("ingame"));
+
+// Choices :
+const choices = document.querySelectorAll(".object");
+
+choices.forEach((choice) =>
+    choice.addEventListener("click", function () {
+        if (document.querySelector(".object.active") !== null) {
+            document.querySelector(".object.active").classList.remove("active");
+        }
+        this.classList.add("active");
+    })
+);
