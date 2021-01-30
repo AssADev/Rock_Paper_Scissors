e2c8785f18b35ca48b4c8fc25b3ee7e391d95d9f
AssADev <gravelteo.pro@gmail.com>
2021-01-30 13:13:43 +0100

    Final correction

--- files/js/app.js
+++ files/js/app.js
@@ -6,17 +6,19 @@ let userScoreboard = document.getElementById("userscoreboard");
 let computerScoreboard = document.getElementById("computerscoreboard");
 
 const choices = document.querySelectorAll(".object");
-const computer_pick = ["Meteorite", "Star", "Rocket"];
+const computerPick = ["Meteorite", "Star", "Rocket"];
 
 let computerChoiceTitle = document.querySelector(".computer_choice h3");
+let computerChoiceImg = document.querySelector(".computer_choice img");
 let lastComputerChoice = document.querySelector(".last_choice h3");
 let lastChoice = "";
 let updateComputerRound = 0;
 
-const start_round_btn = document.getElementById("launch_round");
-const reset_btn = document.getElementById("reset_btn");
+const startRoundBtn = document.getElementById("launch_round");
+const startHomeBtn = document.querySelector("a[href='#game']");
+const resetBtn = document.getElementById("reset_btn");
 
-const actions_logs = document.querySelector(".actions");
+const actionsLogs = document.querySelector(".actions");
 
 // Logs :
 let logs = {
@@ -38,7 +40,7 @@ choices.forEach((choice) =>
 );
 
 // Play Function :
-start_round_btn.addEventListener("click", () => {
+startRoundBtn.addEventListener("click", () => {
     if (document.querySelector(".object.active") == null || userScore >= 5 || computerScore >= 5) {
         return;
     }
@@ -50,7 +52,7 @@ start_round_btn.addEventListener("click", () => {
 
     if (userChoice === computerChoice) {
         infos_logs = `${logs.equal_round} (${userChoice} - ${computerChoice})`;
-        actions_logs.innerHTML += `<p><i class="fas fa-equals"></i> ${infos_logs}</p>`;
+        actionsLogs.innerHTML += `<p><i class="fas fa-equals"></i> ${infos_logs}</p>`;
     }
     if (
         (userChoice === "Meteorite" && computerChoice === "Rocket") ||
@@ -58,7 +60,7 @@ start_round_btn.addEventListener("click", () => {
         (userChoice === "Rocket" && computerChoice === "Star")
     ) {
         infos_logs = `${logs.win_round} (${userChoice} - ${computerChoice})`;
-        actions_logs.innerHTML += `<p><i class="fas fa-check"></i> ${infos_logs}</p>`;
+        actionsLogs.innerHTML += `<p><i class="fas fa-check"></i> ${infos_logs}</p>`;
         counterWin();
     }
     if (
@@ -67,16 +69,17 @@ start_round_btn.addEventListener("click", () => {
         (userChoice === "Star" && computerChoice === "Rocket")
     ) {
         infos_logs = `${logs.lose_round} (${userChoice} - ${computerChoice})`;
-        actions_logs.innerHTML += `<p><i class="fas fa-times"></i> ${infos_logs}</p>`;
+        actionsLogs.innerHTML += `<p><i class="fas fa-times"></i> ${infos_logs}</p>`;
         counterLose();
     }
 
     updateComputer(computerChoice);
+    scrollBottom();
 });
 
 // Check choices :
 function computerPlay() {
-    return computer_pick[Math.floor(Math.random() * computer_pick.length)];
+    return computerPick[Math.floor(Math.random() * computerPick.length)];
 }
 
 function userPlay() {
@@ -93,7 +96,7 @@ function counterWin() {
     userScore++;
     userScoreboard.innerText = userScore;
     if (userScore == 5) {
-        actions_logs.innerHTML += `<p><i class="fas fa-user-astronaut"></i> ${logs.win_game} (${userScore} - ${computerScore})</p>`;
+        actionsLogs.innerHTML += `<p><i class="fas fa-user-astronaut"></i> ${logs.win_game} (${userScore} - ${computerScore})</p>`;
     }
 }
 
@@ -101,7 +104,7 @@ function counterLose() {
     computerScore++;
     computerScoreboard.innerText = computerScore;
     if (computerScore == 5) {
-        actions_logs.innerHTML += `<p><i class="fas fa-robot"></i> ${logs.lose_game} (${userScore} - ${computerScore})</p>`;
+        actionsLogs.innerHTML += `<p><i class="fas fa-robot"></i> ${logs.lose_game} (${userScore} - ${computerScore})</p>`;
     }
 }
 
@@ -109,6 +112,8 @@ function counterLose() {
 function updateComputer(computerChoice) {
     computerChoiceTitle.innerHTML = computerChoice;
     computerChoiceTitle.id = computerChoice.toLowerCase();
+
+    computerChoiceImg.src = `img/${computerChoice.toLowerCase()}.png`;
 
     if (updateComputerRound != 0) {
         lastComputerChoice.innerHTML = lastChoiceText;
@@ -120,8 +125,26 @@ function updateComputer(computerChoice) {
 }
 
 // Reset :
-reset_btn.addEventListener("click", () => {
+startHomeBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+function resetGame() {
+    document.querySelector(".object.active").classList.remove("active");
+
     [userScore, computerScore] = [0, 0];
     [userScoreboard.innerText, computerScoreboard.innerText] = [userScore, computerScore];
-    actions_logs.innerHTML = "";
-});
+    actionsLogs.innerHTML = "";
+
+    computerChoiceImg.src = "img/question_mark.png";
+
+    computerChoiceTitle.innerHTML = "Let's play";
+    computerChoiceTitle.id = "letplay";
+
+    lastComputerChoice.innerHTML = "Nothing";
+    lastComputerChoice.id = "nothing";
+}
+
+// ScrollBottom :
+function scrollBottom() {
+    actionsLogs.scrollTop = actionsLogs.scrollHeight;
+}
